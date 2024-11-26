45e6b5f99f0d6e4a5c1832cb2e026556722a7c1a
Pratap <pratapmisalngu@gmail.com>
2024-11-26 23:30:59 +0530

    Update server.js
    updated

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,12 @@ const PORT = process.env.PORT || 5000 ;
 
 
 app.get("/", (req,res)=>{
+    
+});
 
-})
+app.get("/api/auth/signup", (req,res)=>{
+    
+});
 
 
 app.listen(PORT, ()=> console.log(`Server running on PORT ${PORT}`));
