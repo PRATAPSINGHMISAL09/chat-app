c337a4ef756e0a4f60fe1a28c7924e755f0b54e2
Pratap <pratapmisalngu@gmail.com>
2024-11-29 15:36:25 +0530

    data going into mongodb

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,8 +1,41 @@
+import User from "../models/user.model.js";
+
 export const signup = async (req,res) =>{
     try {
         const {fullname , username , password , confirmpassword , gender } = req.body;
+        if(password !== confirmpassword){
+            return res.status(400).json("Passwords do not match");
+        }
+
+        const user = await User.findOne({username});
+
+        if(user){
+            return res.status(400).json("Username already exists");
+        }
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        const newUser = new User({
+            fullname,
+            username,
+            password,
+            gender,
+            profilepic: gender === 'male' ? boyProfilePic : girlProfilePic
+        })
+
+        await newUser.save();
+
+        res.status(201).json({
+            _id : newUser._id,
+            fullname:newUser.fullname,
+            username:newUser.username,
+            profilepic : newUser.profilepic
+        })
+
     } catch (error) {
-        
+        console.log("Error in signup controller",error.message);
+        res.status(500).json({error:"Internal Server Error"});
     }    
 }
 
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,20 @@
 import express from "express"
 import dotenv from "dotenv"
+
 import authRoutes from "./routes/auth.routes.js"
 import connectmongodb from "./db/connectmongodb.js";
+
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000 ;
+dotenv.config();
 
-
-app.get("/", (req,res)=>{
-    
-});
+app.use(express.json());
 
 app.use("/api/auth",authRoutes);
+
+// app.get("/", (req,res)=>{
+    
+// });
 
 app.listen(PORT, ()=> {
     connectmongodb();
