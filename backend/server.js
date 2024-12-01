85f44f2a103ed6efefec3984740f06b04c4a62d4
Pratap <pratapmisalngu@gmail.com>
2024-12-01 17:10:41 +0530

    Added user routes and controller

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
+
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectmongodb from "./db/connectmongodb.js";
 
 const app = express();
@@ -14,6 +18,7 @@ app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+app.use("/api/users",userRoutes);
 
 
 // app.get("/", (req,res)=>{
