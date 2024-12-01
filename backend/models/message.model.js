0e9d95406ef96b96b77384f17c0b3b8b7c19db9b
Pratap <pratapmisalngu@gmail.com>
2024-12-01 16:25:33 +0530

    messages now are stored into the DB

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema({
     senderId:{
-        type:mongoose.Types.ObjectId,
+        type: mongoose.Types.ObjectId,
         ref: "User",
         required: true
 
     },
     receiverId:{
-        type:mongoose.Types.ObjectId,
+        type: mongoose.Types.ObjectId,
         ref:"User",
         required:true
     },
