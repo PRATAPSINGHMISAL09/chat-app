58441de4462554bdf291b7b996a544571b22a298
Pratap <pratapmisalngu@gmail.com>
2025-03-06 19:01:14 +0530

    socket implementation in backend

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,10 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectmongodb from "./db/connectmongodb.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000 ;
+
 dotenv.config();
 
 app.use(express.json());
@@ -25,7 +26,7 @@ app.use("/api/users",userRoutes);
     
 // });
 
-app.listen(PORT, ()=> {
+server.listen(PORT, ()=> {
     connectmongodb();
     console.log(`Server running on PORT ${PORT}`)
 });
