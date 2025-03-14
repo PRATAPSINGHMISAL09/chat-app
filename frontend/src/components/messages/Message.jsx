c3d39e7ffd184e0e00394f5f0f70dcc18dee75ff
Pratap <pratapmisalngu@gmail.com>
2025-03-14 19:00:03 +0530

    updated bubble component for message

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -9,14 +9,14 @@ function Message({message}) {
   const fromMe = message.senderId === authUser._id;
   const formattedtime = extractTIme(message.createdAt)
   const chatClassName = fromMe ? 'chat-end' : 'chat-start';
-  const profilePic = fromMe ? authUser.profilePic : selectedConversation.profilePic;
+  const profilepic = fromMe ? authUser.profilepic : selectedConversation.profilepic;
   const bubblebgcolor = fromMe ? 'bg-blue-500' : '';
 
   return (
     <div className={`chat ${chatClassName}`}>
         <div className='chat-image avatar'>
             <div className='w-10 rounded-full'>
-                <img src={profilePic} alt="user avatar" />
+                <img src={profilepic} alt="user avatar" />
             </div>
         </div>
       <div className={`chat-bubble text-white ${bubblebgcolor}`}>{message.message}</div>
