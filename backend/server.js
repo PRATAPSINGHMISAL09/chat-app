2b5dc9c537c0d3c4c66eee5fe5fb1c5fa2ef5a9a
Pratap <pratapmisalngu@gmail.com>
2025-03-15 14:11:28 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectmongodb from "./db/connectmongodb.js";
-import { app, io, server } from "./socket/socket.js"; // ✅ Ensure io is imported
+import { app, io, server } from "./socket/socket.js"; 
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -25,6 +27,12 @@ app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+});
 
 server.listen(PORT, () => {
     connectmongodb();
